9a1a75f7a329a15b66f7aaae06b4bbc2474b0ff9
HaoZhu10015 <mrhaozhu@hotmail.com>
2022-08-08 21:42:22 +0200

    Update environment module

---
+++ envs/world/world.py
@@ -0,0 +1,19 @@
+class World:
+    def __init__(self):
+        pass
+
+    def add_task(self):
+        pass
+
+    def add_grid(self):
+        pass
+
+    def step(self):
+        pass
+
+    def reset(self):
+        pass
+
+    def draw(self):
+        pass
+
