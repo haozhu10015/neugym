c023a7917297b958cc8abaffbfac3d964ac3c4f0
HaoZhu10015 <hao.zhu.10015@gmail.com>
2022-08-10 16:13:17 +0200

    Update code

--- test.py
+++ test.py
@@ -1,10 +1,26 @@
-from neugym import environment as env
+import neugym.environment.world as wd
+import neugym.environment.task as tsk
+from neugym.agent import EpsilonGreedyAverager
+
 
 if __name__ == '__main__':
-    world = env.world.SimpleWorld()
+    world = wd.SimpleWorld()
     world.add_task(
-        env.task.Bandit(1, 0.8),
-        env.task.Bandit(1, 0.2)
+        tsk.Bandit(1, 0.7),
+        tsk.Bandit(1, 0.3)
     )
-    world.update_task(0, success_prob=0.5)
-    pass
+
+    agent = EpsilonGreedyAverager(num_action=len(world.actions), init=0.5)
+    q_rec = []
+    for e in range(10000):
+        action = agent.get_action()
+        reward = world.step(action)
+        agent.learn(action=action, reward=reward, lr=0.01)
+
+        if world.time % 1000 == 0:
+            p_0 = world.objects["Tasks"][0].current_success_prob
+            p_1 = world.objects["Tasks"][1].current_success_prob
+            world.update_task(0, success_prob=p_1)
+            world.update_task(1, success_prob=p_0)
+
+        q_rec.append(agent.q.copy())
