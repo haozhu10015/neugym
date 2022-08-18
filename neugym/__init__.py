71a4cbc51f6ceeb849f7f025d500eeeee0a04e63
HaoZhu10015 <mrhaozhu@hotmail.com>
2022-08-18 11:29:06 +0200

    Update documentations

--- neugym/__init__.py
+++ neugym/__init__.py
@@ -3,7 +3,7 @@ NeuGym
 ======
 """
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 
 from neugym.exception import *
--- setup.py
+++ setup.py
@@ -4,13 +4,23 @@ import neugym as ng
 version = ng.__version__
 requirements = open("requirements.txt").readlines()
 
+name = "neugym"
+description = "Python package for reinforcement learning environment of animal behavior modeling."
+authors = {
+    "Hao": ("Hao Zhu", "hao.zhu.10015@gmail.com")
+}
+project_urls = {
+    "Bug Tracker": "https://github.com/HaoZhu10015/neugym/issues"
+}
 setup(
-    name="neugym",
+    name=name,
+    license="LICENSE",
+    readme="README.md",
     version=version,
     author="Hao Zhu",
     author_email="hao.zhu.10015@gmail.com",
     url="https://github.com/HaoZhu10015/neugym",
-    description="Python package for reinforcement learning environment of animal behavior modeling.",
+    description=description,
     packages=find_packages(),
     install_requires=requirements,
     zip_safe=True
