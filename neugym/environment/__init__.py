ad3ac18993600aaa92b90ec36b10a17bbc617ed7
HaoZhu10015 <mrhaozhu@hotmail.com>
2022-08-25 22:49:58 +0200

    Add GridWorldContinuous environment

--- neugym/environment/__init__.py
+++ neugym/environment/__init__.py
@@ -1,5 +1,4 @@
 """Classes for NeuGym environment."""
 
 from .gridworld import *
-# from .delayed_reward_gridworld import *
-# from time_limited_gridworld import *
+# from .gridworld_continuous import *
--- neugym/environment/_object.py
+++ neugym/environment/_object.py
@@ -21,3 +21,11 @@ class _Object:
             self.prob,
             self.coord
         )
+
+
+class _RewardPool:
+    def __init__(self, coord):
+        self.coord: float = coord
+
+    def __repr__(self):
+        return "RewardPool(coord={})".format(self.coord)
